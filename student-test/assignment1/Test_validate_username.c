31f6de577632f036e8589bf0f17eef7dcd47ad52
4trastos <info@4trastos.com>
2025-07-11 07:29:47 +0200

    Assignment 1 completed

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -4,21 +4,15 @@
 #include "../../examples/autotest-validate/autotest-validate.h"
 #include "../../assignment-autotest/test/assignment1/username-from-conf-file.h"
 
-/**
-* This function should:
-*   1) Call the my_username() function in autotest-validate.c to get your hard coded username.
-*   2) Obtain the value returned from function malloc_username_from_conf_file() in username-from-conf-file.h within
-*       the assignment autotest submodule at assignment-autotest/test/assignment1/
-*   3) Use unity assertion TEST_ASSERT_EQUAL_STRING_MESSAGE to verify the two strings are equal.  See
-*       the [unity assertion reference](https://github.com/ThrowTheSwitch/Unity/blob/master/docs/UnityAssertionsReference.md)
-*/
 void test_validate_my_username()
 {
     const char *expected_username = my_username();
     const char *actual_username = malloc_username_from_conf_file();
     
-    TEST_ASSERT_EQUAL_STRING_MESSAGE(expected_username, actual_username, 
-        "El usuario en username.txt no coincide con my_username().");
-    
-    free((void *)actual_username); // Libera la memoria asignada por malloc_username_from_conf_file()
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(
+        expected_username, 
+        actual_username, 
+        "Error: The user in username.txt does not match my_username()."
+    );
+    free((void *)actual_username);
 }
